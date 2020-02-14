13fce2935f05f2cbf8fceebf7cbf8fc45e13817f
Matt-Academia <matias.sidwell@gmail.com>
2020-02-14 17:00:51 -0300

    cambios al user service

--- Tuiter/src/Services/UserService.php
+++ Tuiter/src/Services/UserService.php
@@ -4,27 +4,42 @@ namespace Tuiter\Services;
 
 class UserService {
 
-    private $collection;
+    private $collection = Array();
+    
 
-    public function __construct($collection){
-        $this->collection= $collection;
+    public function __construct(array $collection){
+        $this->collection = $collection;
     }
     
     public function register(string $userId, string $name, string $password) {
+        $changeId=md5($userId);
+        $numero=0;
+        for($i=0;$i<strlen($changeId);$i++){
+            $numero+=ord($changeId[$i]);
+        }
+        $db = $numero % count($this->collection);
         $user = $this->getUser($userId);
         if($user instanceof \Tuiter\Models\UserNull){
             $usuarios= array();
             $usuarios['userId']= $userId;
             $usuarios['name']= $name;
             $usuarios['password']=$password;
-            $this->collection->insertOne($usuarios);
+            $this->collection[$db]->insertOne($usuarios);
+            
             return true;
         } else {
             return false;
         }
     }
     public function getUser($userId){
-        $cursor= $this->collection->findOne(['userId'=> $userId]);
+        $changeId=md5($userId);
+        $numero=0;
+        for($i=0;$i<strlen($changeId);$i++){
+            $numero+=ord($changeId[$i]);
+        }
+        $db = $numero % count($this->collection);    
+        
+        $cursor= $this->collection[$db]->findOne(['userId'=> $userId]);
         if (is_null($cursor)){
             $user = new \Tuiter\Models\UserNull('','','');
             return $user;
